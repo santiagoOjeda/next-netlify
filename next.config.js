e508ff01602be8b9c69c329f94362e533fde9b22
Santi <santiago.ojeda.dm@gmail.com>
2023-03-26 07:36:33 -0300

    wip

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  target: "serverless",
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
